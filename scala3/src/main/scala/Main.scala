23c85614162cec5682f42f32a036761cf65a7c49
roynirmal <nroy000@gmail.com>
2022-12-05 15:03:49 +0100

    Day 3

--- scala3/src/main/scala/Main.scala
+++ scala3/src/main/scala/Main.scala
@@ -2,7 +2,7 @@ import scala.io.Source
 import scala.collection.mutable.ListBuffer
 val filename = "/Users/nirmalroy/Desktop/SearchX/advent-of-code/scala3/data/calorie.txt"
 val rps_file = "/Users/nirmalroy/Desktop/SearchX/advent-of-code/scala3/data/rps.txt"
-
+val rucksack_file = "/Users/nirmalroy/Desktop/SearchX/advent-of-code/scala3/data/rucksack.txt"
 
 @main def maxcalorie: Unit = 
   
@@ -67,3 +67,37 @@ val rps = Map("X" -> 1, "Y" -> 2, "Z" -> 3)
     total += rps_score(line.split(" ")(1))
     }
   println(total)
+
+val small = ('a' to 'z').toList
+val big = ('A' to 'Z').toList
+val full = small ::: big
+@main def ruck_1: Unit = 
+  // println(small ::: big)
+  var output: Int = 0;
+  for(line <- Source.fromFile(rucksack_file).getLines) {
+    val parts = line.grouped(line.length()/2).toList
+    val common = (parts(0) intersect parts(1))(0)
+    output += full.indexOf(common) + 1
+    }
+  println(output)
+
+
+@main def ruck_2: Unit = 
+  val groups = new ListBuffer[String]()
+  var output: Int = 0;
+  var counter: Int = 0;
+  for(line <- Source.fromFile(rucksack_file).getLines) {
+    
+
+    groups += line
+    counter += 1
+    
+    if (counter == 3) {
+      val parts = groups.toList
+      val common = (parts(0) intersect parts(1) intersect parts(2))(0)
+      output += full.indexOf(common) + 1
+      counter = 0
+      groups.clear()
+    }
+  }
+  println(output)
